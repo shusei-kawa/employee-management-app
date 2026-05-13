b3857a376a741a345e7fb13dfdaab2be544b2af9
kawa <kawa@aoiyakk.com>
2026-05-13 12:04:00 +0900

    コメント、変数以外の全ての日本語を英語に変更

--- migration-ojt/src/main/java/com/example/OJT/MigrationOjtApplication.java
+++ migration-ojt/src/main/java/com/example/OJT/MigrationOjtApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class マイクレOjtApplication {
+public class MigrationOjtApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(マイクレOjtApplication.class, args);
+        SpringApplication.run(MigrationOjtApplication.class, args);
     }
 
 }
